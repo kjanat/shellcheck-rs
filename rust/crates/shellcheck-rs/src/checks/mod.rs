778c1ce0b26005a41de2c490a93b8f702105bec8
Kaj Kowalski <info@kajkowalski.nl>
2026-09-11 07:18:23 +0000

    Add batch_i/j/k check module stubs

--- rust/crates/shellcheck-rs/src/checks/mod.rs
+++ rust/crates/shellcheck-rs/src/checks/mod.rs
@@ -1,20 +1,9 @@
 //! Ported check batches. Separate files let batches be developed in parallel.
 use crate::analyzer_lib::Checker;
-pub mod batch_a;
-pub mod batch_b;
-pub mod batch_c;
-pub mod batch_d;
-pub mod batch_e;
-pub mod batch_f;
-pub mod batch_g;
-pub mod batch_h;
+pub mod batch_a; pub mod batch_b; pub mod batch_c; pub mod batch_d; pub mod batch_e;
+pub mod batch_f; pub mod batch_g; pub mod batch_h; pub mod batch_i; pub mod batch_j; pub mod batch_k;
 pub fn register_all(c: &mut Checker) {
-    batch_a::register(c);
-    batch_b::register(c);
-    batch_c::register(c);
-    batch_d::register(c);
-    batch_e::register(c);
-    batch_f::register(c);
-    batch_g::register(c);
-    batch_h::register(c);
+    batch_a::register(c); batch_b::register(c); batch_c::register(c); batch_d::register(c);
+    batch_e::register(c); batch_f::register(c); batch_g::register(c); batch_h::register(c);
+    batch_i::register(c); batch_j::register(c); batch_k::register(c);
 }
